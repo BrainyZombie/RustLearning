9e4afb72117ce512ef6c0cf1c24634b4d8d146bf
Ashir Basu <ashirbasu@gmail.com>
2023-08-19 22:52:32 +0530

    basic input

--- src/mods/a_03_cli_project/a00.rs
+++ src/mods/a_03_cli_project/a00.rs
@@ -1,3 +1,12 @@
 pub fn main(args: Vec<String>) {
-    dbg!(args);
+    dbg!(&args);
+    let query = match args.get(0) {
+        Some(q) => q,
+        None => return,
+    };
+    let file_name = match args.get(1) {
+        Some(q) => q,
+        None => return,
+    };
+    dbg!(file_name, query);
 }
