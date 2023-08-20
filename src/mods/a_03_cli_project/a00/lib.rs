92bd75cda93da767616474d03ae0b7e71772dc3e
Ashir Basu <ashirbasu@gmail.com>
2023-08-20 11:14:53 +0530

    first version working

--- src/mods/a_03_cli_project/a00/lib.rs
+++ src/mods/a_03_cli_project/a00/lib.rs
@@ -6,7 +6,7 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &str> {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 2 {
             return Err("Not enough arguments");
         }
@@ -18,6 +18,35 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_name)?;
-    dbg!(contents);
+    let results = search(&config.query, &contents);
+    for line in results {
+        println!("{line}");
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
