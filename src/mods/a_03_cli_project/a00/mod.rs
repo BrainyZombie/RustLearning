b8f3c5510dc2d7b604292f234d8942b73f4e3936
Ashir Basu <ashirbasu@gmail.com>
2023-08-20 19:12:08 +0530

    use iterators instead

--- src/mods/a_03_cli_project/a00/mod.rs
+++ src/mods/a_03_cli_project/a00/mod.rs
@@ -3,7 +3,7 @@ use lib::{run, Config};
 use std::process;
 
 pub fn main(args: Vec<String>) {
-    let config = Config::build(&args).unwrap_or_else(|e| {
+    let config = Config::build(args.into_iter()).unwrap_or_else(|e| {
         eprintln!("CLI Read error: {e}");
         process::exit(1);
     });
