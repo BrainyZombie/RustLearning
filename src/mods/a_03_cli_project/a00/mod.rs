aa3e618715ae5f5ab1f1b7b38c4173f8b09d95bb
Ashir Basu <ashirbasu@gmail.com>
2023-08-20 11:34:47 +0530

    env variables and stderr

--- src/mods/a_03_cli_project/a00/mod.rs
+++ src/mods/a_03_cli_project/a00/mod.rs
@@ -4,11 +4,11 @@ use std::process;
 
 pub fn main(args: Vec<String>) {
     let config = Config::build(&args).unwrap_or_else(|e| {
-        println!("CLI Read error: {e}");
+        eprintln!("CLI Read error: {e}");
         process::exit(1);
     });
     if let Err(e) = run(config) {
-        println!("Running error: {e}");
+        eprintln!("Running error: {e}");
         process::exit(1);
     }
 }
